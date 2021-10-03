7e6e50c1b139a905dccfa211befbefdfe52fe582
Samik Maharjan <samikmaharjan55@gmail.com>
2021-10-03 16:37:57 +0545

    30

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,15 +9,14 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = 'loading';
-
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.time,
+      'time': instance.time,
     });
   }
 
@@ -32,7 +31,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('loading'),
       ),
     );
   }
