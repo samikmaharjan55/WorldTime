6d4c19bd17ca0a1a49c83de04b4ca3d8e5dae317
Samik Maharjan <samikmaharjan55@gmail.com>
2021-10-03 13:46:56 +0545

    23 5:55

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ));
