6ccc000efe0f9c83b73ac2481472ff75a2e3d8fc
Samik Maharjan <samikmaharjan55@gmail.com>
2021-10-03 14:18:44 +0545

    24

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +27,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Text('choose location screen'),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
