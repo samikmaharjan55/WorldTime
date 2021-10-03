5405e2f5f9ab040ed5ef03cec1546cdfcce69ddc
Samik Maharjan <samikmaharjan55@gmail.com>
2021-10-03 14:09:00 +0545

    23

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home Screen')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: Icon(Icons.edit_location),
+              label: Text('Edit Location'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
